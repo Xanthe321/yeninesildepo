daa0c7c7b3b85294d90b59ed590c99a15d8f4475
Xanthe321 <emrecanozturk16@gmail.com>
2025-09-29 09:51:10 +0300

    #

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import HowToWork from "@/components/HowToWork";
 import Services from "@/components/Services";
@@ -8,12 +7,11 @@ import Storage from "@/components/Storage";
 export default function Home() {
   return (
     <div className="bg-white text-gray-900 font-sans leading-relaxed antialiased">
-      test
-      {/* <HeroSection />
+      <HeroSection />
       <Services />
       <Storage />
       <HowToWork />
-      <CTA /> */}
+      <CTA />
     </div>
   );
 }
